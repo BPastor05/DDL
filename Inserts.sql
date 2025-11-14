a491a4cb0dfd403b4628dd5d68a1771ec4cb64e9
BPastor05 <bpmatute@unah.hn>
2025-11-14 13:57:14 -0600

    Se realizo Inserts de Rutas en la tabla Rutas

---
+++ Inserts.sql
@@ -0,0 +1,47 @@
+Insert into transporte.Ruta(
+origen,
+destino
+)values(
+'Tegucigalpa',
+'Olancho'
+)
+
+Insert into transporte.Ruta(
+origen,
+destino
+)values(
+'Tegucigalpa',
+'San Pedro Sula'
+)
+
+Insert into transporte.Ruta(
+origen,
+destino
+)values(
+'Tegucigalpa',
+'Choluteca'
+)
+
+Insert into transporte.Ruta(
+origen,
+destino
+)values(
+'Olancho',
+'Tegucigalpa'
+)
+
+Insert into transporte.Ruta(
+origen,
+destino
+)values(
+'San Pedro Sula',
+'Tegucigalpa'
+)
+
+Insert into transporte.Ruta(
+origen,
+destino
+)values(
+'Choluteca',
+'Tegucigalpa'
+)
